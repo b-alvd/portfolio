3c8c48aeb22f192436b6c5a695ad06de4b555851
Basile <basilealeveque@gmail.com>
2024-03-21 10:13:48 +0100

    2xClic to open modals

--- src/js/modals.js
+++ src/js/modals.js
@@ -10,28 +10,31 @@ closeButtons.forEach(function(button) {
 
 // FOLDER - INFOS
 var modal_infos = document.getElementById("folder_modal_infos");
-var btn = document.getElementById("open_folder_modal_infos");
-btn.onclick = function() {
+var btn_infos = document.getElementById("open_folder_modal_infos");
+btn_infos.addEventListener('dblclick', function() {
     modal_infos.style.display = "block";
-}
+});
+
 // FOLDER - CAREER
 var modal_career = document.getElementById("folder_modal_career");
-var btn = document.getElementById("open_folder_modal_career");
-btn.onclick = function() {
+var btn_career = document.getElementById("open_folder_modal_career");
+btn_career.addEventListener('dblclick', function() {
     modal_career.style.display = "block";
-}
+});
+
 // FOLDER - PROJECTS
 var modal_projects = document.getElementById("folder_modal_projects");
-var btn = document.getElementById("open_folder_modal_projects");
-btn.onclick = function() {
+var btn_projects = document.getElementById("open_folder_modal_projects");
+btn_projects.addEventListener('dblclick', function() {
     modal_projects.style.display = "block";
-}
+});
+
 // FOLDER - CONTACT
 var modal_contact = document.getElementById("folder_modal_contact");
-var btn = document.getElementById("open_folder_modal_contact");
-btn.onclick = function() {
+var btn_contact = document.getElementById("open_folder_modal_contact");
+btn_contact.addEventListener('dblclick', function() {
     modal_contact.style.display = "block";
-}
+});
 
 // WINDOWS
 var modal_windows = document.getElementById("modal_windows");
@@ -47,7 +50,7 @@ window.onclick = function(event) {
 
 // NOTEPAD - INFOS
 var modal_notepad_infos = document.getElementById("modal_notepad_infos");
-var btn = document.getElementById("open_modal_notepad_infos");
-btn.onclick = function() {
+var btn_notepad_infos = document.getElementById("open_modal_notepad_infos");
+btn_notepad_infos.addEventListener('dblclick', function() {
     modal_notepad_infos.style.display = "block";
-}
+});
