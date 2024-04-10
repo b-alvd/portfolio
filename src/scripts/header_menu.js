797b643d1b7beb8f38e573321c02dc348314c451
Basile <basilealeveque@gmail.com>
2024-04-10 23:36:13 +0200

    [mobile] header nav menu

--- src/scripts/header_menu.js
+++ src/scripts/header_menu.js
@@ -1,5 +1,7 @@
-let burger_button = document.querySelector('.header_burger_button')
+let burger_button = document.querySelector('.header_burger_button');
+let nav_menu = document.querySelector('header #header_nav nav ul');
 
 burger_button.addEventListener('click', () => {
-    burger_button.classList.toggle('header_burger_button--active')
+    burger_button.classList.toggle('header_burger_button--active');
+    nav_menu.classList.toggle('ul--open');
 });
