17b82696ea8c610452e9780b6ab4887220b66d0a
Basile <basilealeveque@gmail.com>
2024-04-11 10:09:26 +0200

    modifs on header nav transition

--- src/scripts/header_menu.js
+++ src/scripts/header_menu.js
@@ -2,6 +2,10 @@ let burger_button = document.querySelector('.header_burger_button');
 let nav_menu = document.querySelector('header #header_nav nav ul');
 
 burger_button.addEventListener('click', () => {
-    burger_button.classList.toggle('header_burger_button--active');
-    nav_menu.classList.toggle('ul--open');
+    nav_menu_open()
 });
+
+function nav_menu_open() {
+    nav_menu.classList.toggle('ul--open');
+    burger_button.classList.toggle('header_burger_button--active');
+}
